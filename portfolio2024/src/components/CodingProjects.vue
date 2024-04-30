727b14ecf26f0991b3297b5506f103aca41752e1
ATaylorN <amandanagies@yahoo.com>
2024-04-29 23:00:25 -0400

    edits to coding section

--- portfolio2024/src/components/CodingProjects.vue
+++ portfolio2024/src/components/CodingProjects.vue
@@ -17,22 +17,12 @@
             <img src="@/assets/icons/vscode.png" alt="VS Code icon">
             <img src="@/assets/icons/css.png" alt="CSS icon">
             <img src="@/assets/icons/pgadmin.png" alt="PGAdmin icon">
-            <p class="card-text"> 12 weeks into the program, we were assigned to a team of 4. Together we utilized Java, Spring Boot, PostgreSQL, and Vue.js 
-                (just to name a few) to develop a meal planner web application where a user can view, add, or update recipes with ease. In the 'Build a Recipe' 
-                page, the user can search for ingredients to add to their recipe and the information/images are supplied by an API. Our Calendar contained a 
-                lot of functionality; by simply dragging and dropping selected recipes onto the calendar, users can plan out a full month's worth of meals 
-                accurately. With the option to select a date range, the application will display the necessary ingredients needed for that period. To 
-                further streamline the process, users can print or email the grocery list, and even download a VCS file that is compatible with Google Calendar.<br>
-
-                <br><b>What I Learned:</b><br>
-                In this project, I learned a lot about team dynamics and the importance of communication. Some of my 
-                team members were people I had never met before the project since they were from another class. We also
-                 had half of the team working remote, but we were still able to produce an awesome product. Throughout 
-                 this process, I feel like I never stopped learning. We ran into a couple of roadblocks in the beginning 
-                 because we were eager to get to coding, but once we took the time to thoroughly design out what we wanted 
-                 the application to be, we were able to execute far more efficiently. I personally learned a ton about 
-                 frontend including HTML, CSS, and Vue.js. As a result, in just 2 weeks, we were able to develop an app 
-                 that had quite a few features, looked amazing, and is something that we all feel proud of.</p>
+            <p class="card-text">Utilized Java, Spring Boot, PostgreSQL, & Vue.js to develop a meal planner web application 
+                where a user can view, add, & update recipes with ease. In the 'Build a Recipe' 
+                page, the user can search for ingredients & the information/images are supplied by an API. By simply dragging & dropping 
+                selected recipes onto the calendar, users can plan out a full month's worth of meals. 
+                With the option to select a date range, the application will display the necessary ingredients needed for that period. To 
+                further streamline the process, users can print or email the grocery list, & even download a ICS file that is compatible with Google Calendar.</p>
             <a href="https://github.com/ATaylorN/capstone3_mealplanner" class="btn">View Project</a>
             </div>
         </div>
@@ -51,20 +41,13 @@
             <img src="@/assets/icons/vscode.png" alt="VS Code icon">
             <img src="@/assets/icons/css.png" alt="CSS icon">
             <img src="@/assets/icons/pgadmin.png" alt="PGAdmin icon">
-            <p class="card-text">This project was actually heavily inspired by a fellow classmate who created something
-                 similar for his .NET studying and I wanted to do something similar for Java. Using Java, Spring Boot, 
+            <p class="card-text">Using Java, Spring Boot, 
                  and PostgreSQL for the backend and Vue.js for the frontend, I created an app where the user can choose 
                  to have random flashcards to cycle through or chose "Study Mode". "Study Mode" allows a user to cycle 
                  through questions and mark whether or not they got the question right. When the user opts to exit, the
                   user is shown a list of all of the questions that they got incorrect so that they know what they need
                    to continue studying. There's also an option for the user to really put their knowledge to the test
-                    with a quiz.<br>
-
-                <br><b>What I Learned:</b><br>
-                This project is great because I continue to learn from it even after completing it. Not only was it a 
-                great solo learning experience where I learned things like how to pull random data from a database and
-                 how to make quizzes, but I still go back to it to study. I can also easily add new questions to study
-                  as I continue to learn new things..</p>
+                    with a quiz.</p>
             <a href="https://github.com/ATaylorN/flashcards" class="btn">View Project</a>
             </div>
         </div>
@@ -101,20 +84,12 @@
             <img src="@/assets/icons/vscode.png" alt="VS Code icon">
             <img src="@/assets/icons/css.png" alt="CSS icon">
             <img src="@/assets/icons/pgadmin.png" alt="PGAdmin icon">
-            <p class="card-text"> Status: In Progress<br>
-
-                <br><b>Goal:</b><br>
-                I have currently built out the database that I will be using for this project. I at one point 
-                had the SQL queries and what not started in IntelliJ, but it somehow managed to get deleted. 
-                I'm a little disappointed, but I've learned a lot since I started this project so maybe it's 
-                ultimately for the best. The end goal for this project is to have an application where I, as a 
-                user, can have a library catalog of all of the books that I own. I want to have the ability to 
-                view them based on whether or not they've been read yet and I want to be able to browse them based
+            <p class="card-text">
+                 The end goal is to have an application where I can have a library catalog of all of the books that I own. I want to have the ability to 
+                view them based on whether or not they've been read and be able to browse them based
                  on things like genre and series. I'd also like to add a wishlist component to it so that I can keep
-                  track of books that I'm interested in purchasing and have it keep a running total of how much these
-                   books cost. Honestly, I'm tired of going to a bookstore, picking up a book and wondering if it looks 
-                   familiar because I've considered purchasing it before or because I've already purchased it and it's
-                    at home waiting to be read.</p>
+                  track of books that I'm interested in purchasing and have it keep a running total of price. 
+                 </p>
             <a href="https://github.com/ATaylorN/library_side_project" class="btn">View Project</a>
             </div>
         </div>
